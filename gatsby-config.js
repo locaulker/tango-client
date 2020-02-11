06a15dc5e9046921f90f9180449795d23145cd90
Lawrence <Caulker>
2020-02-11 07:51:22 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tango Brand Alliance`,
+    description: `Tango Brand Alliance Website`,
+    author: `@locaulker`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,39 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        excludedRoutes: ['/wp/v2/users/**', '/wp/v2/settings*', '/wp/v2/themes*'],
+        baseUrl: "tango-data.test",
+        protocol: "http",
+        hostingWPCOM: false,
+        useACF: true,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "http://tango-data.test",
+          replacementUrl: "",
+        },
+      }
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Teko`,
+            variants: [`300`, `400`, `500`, `600`, `700`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`400`, `700`]
+          },
+        ],
+      },
+    }
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
